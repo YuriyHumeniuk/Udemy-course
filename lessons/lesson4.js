eb5022c255a739cc07a6296f0690c59ada99c180
Yuriy-Humeniuk <yhumeniuk@seluxdx.com>
2025-02-05 16:32:00 +0200

    lesson4_strict

--- lessons/lesson4.js
+++ lessons/lesson4.js
@@ -2,3 +2,10 @@
 
 var result = 5 <= 5
 console.log(result)
+
+//equalit operators
+
+var x = 1
+console.log(x == '1') //lose comparison value on the left compare tovalueon the right, not comparing the data type
+console.log(x === '1') // FALSE - strict comparision, not only value compare but type as well - 1 as a number not equel to '1' as a string
+console.log(x === 1) // TRUE - strict comparision, not only value compare but type as well - 1 as a number not equel to '1' as a string
